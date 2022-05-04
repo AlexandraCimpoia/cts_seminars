93bf730f9b6e6f536e5640ac9ce59f59c8cdd32f
AlexandraCimpoia <81245945+AlexandraCimpoia@users.noreply.github.com>
2022-05-04 15:05:41 +0300

    Add files via upload

--- Seminar_5/src/builder_solved/ProgMain.java
+++ Seminar_5/src/builder_solved/ProgMain.java
@@ -3,13 +3,12 @@ package builder_solved;
 public class ProgMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		User u1=new User("diana@gmail.com","diana@gmail.com","diana", "b","bucharest");
-//		User u2=new User("diana@gmail.com","diana@gmail.com","diana", "b","0740123456");
-		
-		User u= new User.UserBuilder("anca@gmail.com","anca@gmail.com","anca", "b").address("Bucharest").build();
-		User u2= new User.UserBuilder("anca@gmail.com","anca@gmail.com","anca", "b").phone("0740123456").build();
 
+		User u = new User.UserBuilder("a@gmail.com", "a@gmail.com", "a", "b").address("Bucharest").build();
+		User u2 = new User.UserBuilder("a@gmail.com", "a@gmail.com", "a", "b").phone("0740123456").build();
+
+		System.out.println(u);
+		System.out.println(u2);
 	}
 
 }
--- Seminar_5/src/builder_solved/User.java
+++ Seminar_5/src/builder_solved/User.java
@@ -1,72 +1,76 @@
-package builder.solved;
+package builder_solved;
 
 public class User {
-	
-	//mandatory
+
+	// mandatory
+	private String username;
+	private String email;
+	private String firstName;
+	private String lastName;
+
+	// optional
+	private String phoneNo;
+	private String address;
+	private String birthday;
+
+	public User(UserBuilder u) {
+		// mandatory
+		this.username = u.username;
+		this.email = u.email;
+		this.firstName = u.firstName;
+		this.lastName = u.lastName;
+
+		// optional
+		this.address = u.address;
+		this.birthday = u.birthday;
+		this.phoneNo = u.phoneNo;
+
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", phoneNo=" + phoneNo + ", address=" + address + ", birthday=" + birthday + "]";
+	}
+
+	public static class UserBuilder {
+
+		// mandatory
 		private String username;
 		private String email;
 		private String firstName;
 		private String lastName;
 
-	//optional
+		// optional
 		private String phoneNo;
 		private String address;
 		private String birthday;
-		
-		public User(UserBuilder u) {
-			//mandatory
-			this.username=username;
-			this.email=email;
-			this.firstName=firstName;
-			this.lastName=lastName;
-			
-			//optional
-			this.address=u.address;
-			this.birthday=u.birthday;
-			this.phoneNo=u.phoneNo;
-			
-		}
-		
-		public static class UserBuilder{
-			
-	//mandatory
-			private String username;
-			private String email;
-			private String firstName;
-			private String lastName;
 
-	//optional
-			private String phoneNo;
-			private String address;
-			private String birthday;
-			
-			public UserBuilder(String username, String email, String firstName, String lastName) {
-				this.username=username;
-				this.email=email;
-				this.firstName=firstName;
-				this.lastName=lastName;
-			}
-			
-			public UserBuilder birthday(String birthday) {
-				this.birthday=birthday;
-				return this;
-			}
-			
-			public UserBuilder address(String address) {
-				this.address=address;
-				return this;
-			}
-			
-			public UserBuilder phone(String phoneNo) {
-				this.phoneNo=phoneNo;
-				return this;
-			}
-			
-			public User build() {
-				User u=new User(this);
-				return u;
-			}
+		public UserBuilder(String username, String email, String firstName, String lastName) {
+			this.username = username;
+			this.email = email;
+			this.firstName = firstName;
+			this.lastName = lastName;
 		}
 
+		public UserBuilder birthday(String birthday) {
+			this.birthday = birthday;
+			return this;
+		}
 
+		public UserBuilder address(String address) {
+			this.address = address;
+			return this;
+		}
+
+		public UserBuilder phone(String phoneNo) {
+			this.phoneNo = phoneNo;
+			return this;
+		}
+
+		public User build() {
+			User u = new User(this);
+			return u;
+		}
+	}
 }
